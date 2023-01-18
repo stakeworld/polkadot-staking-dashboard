c976e3ac893ae78e4a685ee923972657c35af01f
Ross Bulat <ross@parity.io>
2023-01-18 14:52:22 +0700

    meta

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ import { stringToU8a } from '@polkadot/util';
  * Global Constants
  */
 export const AppVersion = '1.0.3';
-export const UriPrefix = '/';
+export const UriPrefix = '/polkadot-staking-dashboard';
 export const DappName = 'Polkadot Staking Dashboard';
 export const PolkadotUrl = 'https://polkadot.network/staking/';
 export const DefaultNetwork = 'polkadot';
